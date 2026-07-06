9d3793ecb6ec815ff0e6f724f9b42142aa1c9e7b
jv8gbf476b-ctrl <jv8gbf476b@privaterelay.appleid.com>
2026-07-06 14:44:36 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,23 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
     return render_template("index.html")
 
+
+@app.route("/health")
+def health():
+    return {
+        "status": "running",
+        "app": "Movie_AI"
+    }
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=5000,
+        debug=True
+    )

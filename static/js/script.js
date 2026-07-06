7d35cd0355bdd36cb351e01a914911384e4d7d16
jv8gbf476b-ctrl <jv8gbf476b@privaterelay.appleid.com>
2026-07-06 14:38:17 +0900

    Create script.js

---
+++ static/js/script.js
@@ -0,0 +1,22 @@
+document
+.getElementById("generate")
+.onclick=()=>{
+
+const scene=document
+.getElementById("scene")
+.value;
+
+document
+.getElementById("result")
+.value=
+
+`Create a cinematic video.
+
+Scene:
+${scene}
+
+Realistic.
+Natural movement.
+Movie quality.
+`;
+}
